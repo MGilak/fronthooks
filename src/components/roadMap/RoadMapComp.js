f2622ae85798bc194783547e717d700372684df1
gilakrz <mahmoud.gilak@yahoo.com>
2023-07-08 15:05:53 +0330

    roadMap is don

--- src/components/roadMap/RoadMapComp.js
+++ src/components/roadMap/RoadMapComp.js
@@ -1,25 +1,13 @@
 import React from "react";
 
-const RoadMapComp = ({childer}) => {
+const RoadMapComp = ({ children, info }) => {
+  console.log(children);
   return (
     <div className="text-secondary-700 hover:text-blue-500 flex items-center flex-col transition-all duration-200 ease-in">
-      <div className="bg-white w-24 h-24 rounded-[41px] flex justify-center items-center">
-        <svg
-          stroke="currentColor"
-          fill="currentColor"
-          stroke-width="0"
-          role="img"
-          viewBox="0 0 24 24"
-          class="h-9 w-9 md:w-10 md:h-10 z-10"
-          height="1em"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title></title>
-          <path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"></path>
-        </svg>
+      <div className="bg-white md:w-24 md:h-24 w-20 h-20 rounded-[41px] flex justify-center items-center">
+        {children}
       </div>
-      <p className="text-sm mt-4 font-bold">HTML CSS</p>
+      <p className="text-sm mt-4 font-bold">{info}</p>
     </div>
   );
 };
